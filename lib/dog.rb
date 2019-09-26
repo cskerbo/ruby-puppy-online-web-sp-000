aa42025f81f95f635f38d01e73f503f6dba318d3
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-26 02:47:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,15 @@
 class Dog
 
-attr_accessor :name
+  attr_accessor :name
 
-@@all = []
+  @@all = []
 
-def initialize(name)
-  @name = name
-end
+  def initialize(name)
+    @name = name
+  end
 
-def self.all
-  @@all
+  def self.all
+    @@all
+  end
+
 end
